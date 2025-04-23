35eeb5d612d85f26a8a19601bdef4a1de0db6ef8
Ehelsan <ehelsan@ehelsans-air.home>
2025-04-23 21:30:43 +0200

    added list to display saved bookmarks

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 let myBookmarks = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+const listEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function(){
+  myBookmarks = []
   myBookmarks.push(inputEl.value)
+  for (let i = 0; i < myBookmarks.length; i++){
+    listEl.innerHTML += "<li>" + myBookmarks[i] + "</li>"
+  }
 })
 
