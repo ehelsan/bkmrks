21f3dd13343cf5a9b419f812b78b3c6791a9fce0
Ehelsan <ehelsan@ehelsans-air.home>
2025-04-27 21:26:51 +0200

    added click functionality to bookmarks list which opens to a new tab & refactored code

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ const inputBtn = document.querySelector("#input-btn")
 const listEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function(){
-  myBookmarks = []
   myBookmarks.push(inputEl.value)
-  for (let i = 0; i < myBookmarks.length; i++){
-    listEl.innerHTML += "<li>" + myBookmarks[i] + "</li>"
-  }
+  inputEl.value = ""
+  renderList()
 })
 
+function renderList(){
+  let listItems = ""
+  for (let i = 0; i < myBookmarks.length; i++){
+    listItems += `
+    <li>
+      <a target='_blank' href='${myBookmarks[i]}'> 
+        ${myBookmarks[i]}
+      </a>
+    </li>
+    `
+  }
+  listEl.innerHTML = listItems
+}
