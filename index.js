4bd77bafd322b68811ff5b20d5b8cec88fb3c51a
Ehelsan <ehelsan@ehelsans-air.home>
2025-05-11 18:56:22 +0200

    added delete bookmarks and save tab functionality

--- index.js
+++ index.js
@@ -2,29 +2,46 @@
 let myBookmarks = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+const deleteBtn = document.querySelector("#delete-btn")
+const tabBtn = document.querySelector("#tab-btn")
 const listEl = document.querySelector("#ul-el")
 
 let bookmarksFromLocalStorage = JSON.parse(localStorage.getItem("myBookmarks"))
 
-if (bookmarksFromLocalStorage) {
-  myBookmarks = bookmarksFromLocalStorage
-  renderList()
-}
 
 inputBtn.addEventListener("click", function(){
   myBookmarks.push(inputEl.value)
   inputEl.value = ""
   localStorage.setItem("myBookmarks", JSON.stringify(myBookmarks))
-  renderList()
+  render(myBookmarks)
 })
 
-function renderList(){
+deleteBtn.addEventListener("click", function(){
+  localStorage.clear()
+  myBookmarks = []
+  render(myBookmarks)
+})
+
+tabBtn.addEventListener("click", function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    myBookmarks.push(tabs[0].url)
+    localStorage.setItem("myBookmarks", JSON.stringify(myBookmarks))
+    render(myBookmarks)
+  })
+})
+
+if (bookmarksFromLocalStorage) {
+  myBookmarks = bookmarksFromLocalStorage
+  render(myBookmarks)
+}
+
+function render(bookmarks){
   let listItems = ""
-  for (let i = 0; i < myBookmarks.length; i++){
+  for (let i = 0; i < bookmarks.length; i++){
     listItems += `
     <li>
-      <a target='_blank' href='${myBookmarks[i]}'> 
-        ${myBookmarks[i]}
+      <a target='_blank' href='${bookmarks[i]}'> 
+        ${bookmarks[i]}
       </a>
     </li>
     `
